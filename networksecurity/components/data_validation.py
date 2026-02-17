9ffcac4a5b016c7821697e3283569acd8aafc7f0
Santhosh <santhoshaloori1314@gmail.com>
2026-02-17 17:17:25 -0500

    fixed data validation code

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -60,7 +60,7 @@ class DataValidation:
             dir_path = os.path.dirname(drift_report_file_path)
             os.makedirs(dir_path, exist_ok=True)
             write_yaml_file(file_path=drift_report_file_path, content=report)
-            return status  # ✅ Fixed: was missing return statement
+            return status 
 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
